cf11b95d8b00f9c355abd507ac496fc885298133
rjganatra <69088751+rjganatra@users.noreply.github.com>
2026-05-10 11:31:58 +0530

    Add files via upload

---
+++ app/utils/cache.py
@@ -0,0 +1,25 @@
+
+import pickle
+from pathlib import Path
+
+CACHE_DIR = "data/cache"
+
+Path(CACHE_DIR).mkdir(
+    parents=True,
+    exist_ok=True
+)
+
+def save_cache(name, data):
+
+    with open(f"{CACHE_DIR}/{name}.pkl", "wb") as f:
+        pickle.dump(data, f)
+
+def load_cache(name):
+
+    path = f"{CACHE_DIR}/{name}.pkl"
+
+    if not Path(path).exists():
+        return None
+
+    with open(path, "rb") as f:
+        return pickle.load(f)
